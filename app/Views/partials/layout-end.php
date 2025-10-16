1e031eec2b51dd75e2931f0b19b2e20f129531b6
Orlan Rocha <orlan.rocha@outlook.com>
2025-10-15 22:47:13 -0300

    Fix CDN SRI hashes and include jQuery dependency

--- app/Views/partials/layout-end.php
+++ app/Views/partials/layout-end.php
@@ -1,6 +1,7 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-zuk7tAEvtTDHkJ/xZBwbNG0Ax7AnCM1j403rwhhtjfiPgk41IrT9jp+1rssZCvGS" crossorigin="anonymous" defer></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-7g1cR0RvrRZtGZv00zMNTUMlzUxr87Pl13V4Aa9u0nM=" crossorigin="anonymous" defer></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js" defer></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-LInM/apm0M1f9bBlt3F4iAfDdc0AGJi/7luWGINuD/7++UZ5EKeosFVJeFt3PcTJS3BM4tiTqcKki0VZZng9BA==" crossorigin="anonymous" referrerpolicy="no-referrer" defer></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer" defer></script>
 <script src="https://cdn.datatables.net/v/bs5/dt-2.0.3/datatables.min.js" defer></script>
 <script src="/js/app.js" type="module"></script>
 </body>
--- app/Views/partials/layout-start.php
+++ app/Views/partials/layout-start.php
@@ -13,11 +13,11 @@ $pageTitle = $pageTitle ?? 'WhatsAtende';
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-JgEdRkNvztNFVQVw1Gc7YCOUMIqFZRMVAbwYSEuxsjjXNMTvEfgQ6VbUaz2QHCcm" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/v/bs5/dt-2.0.3/datatables.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/@sweetalert2/theme-bootstrap-4@5.0.12/bootstrap-4.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet" integrity="sha512-vKM265p3nSUQe8Vn0tH3UX0DpD+s/COM24kTx5cDIeEJD7BqXc9E+u6KDAdAm8YGtS+wGGyRlmE7CqCtaA+1mw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet" integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="/css/theme.css" rel="stylesheet">
 </head>
 <body class="app-body">
