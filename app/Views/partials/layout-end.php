f73a251c5397ed4aa5c5c4a774529e956da09c0b
Orlan Rocha <orlan.rocha@outlook.com>
2025-10-15 23:57:03 -0300

    Fix jQuery SRI hash

--- app/Views/partials/layout-end.php
+++ app/Views/partials/layout-end.php
@@ -1,4 +1,4 @@
-<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-7g1cR0RvrRZtGZv00zMNTUMlzUxr87Pl13V4Aa9u0nM=" crossorigin="anonymous" defer></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous" defer></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js" defer></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer" defer></script>
